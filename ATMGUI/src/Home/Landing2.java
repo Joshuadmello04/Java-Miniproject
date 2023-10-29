078d2a0e3ec4ccf9fb53ff8d93e1b468701f7c58
Joshuadmello04 <124428153+Joshuadmello04@users.noreply.github.com>
2023-10-29 15:17:32 +0530

    backspace and deposit
    everythings done fr this time fr

--- ATMGUI/src/Home/Landing2.java
+++ ATMGUI/src/Home/Landing2.java
@@ -3,19 +3,20 @@ package Home;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 import java.sql.*;
+
+import javax.swing.*;
 
 public class Landing2 extends JFrame implements ActionListener {
     private JButton backButton;
+    private JButton depositButton;
     private JButton withdrawButton;
     private JTextField withdrawalAmountField;
-
     private JButton balanceButton;
     private Font customFont = new Font("Tahoma", Font.PLAIN, 18);
     private Color primaryColor = new Color(68, 108, 179);
-
     private JButton[] digitButtons;
+    private JButton backspaceButton;
     private String inputBuffer = "";
     private String username;
     private String password;
@@ -29,6 +30,9 @@ public class Landing2 extends JFrame implements ActionListener {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
         setSize(800, 600);
+
+        int centerX = (800 - 3 * 60) / 2;
+        int centerY = 250;
 
         JPanel contentPane = new JPanel() {
             @Override
@@ -44,12 +48,18 @@ public class Landing2 extends JFrame implements ActionListener {
         contentPane.setLayout(null);
         setContentPane(contentPane);
 
-        backButton = createStyledButton("Back");
+        backButton = new JButton("Back");
+        backButton.setFont(customFont);
+        backButton.setBackground(primaryColor);
+        backButton.setForeground(Color.WHITE);
         backButton.setBounds(600, 500, 175, 40);
         backButton.addActionListener(this);
         contentPane.add(backButton);
 
-        balanceButton = createStyledButton("Check Balance");
+        balanceButton = new JButton("Check Balance");
+        balanceButton.setFont(customFont);
+        balanceButton.setBackground(primaryColor);
+        balanceButton.setForeground(Color.WHITE);
         balanceButton.setBounds(600, 360, 175, 40);
         balanceButton.addActionListener(this);
         contentPane.add(balanceButton);
@@ -58,49 +68,66 @@ public class Landing2 extends JFrame implements ActionListener {
         withdrawalAmountField.setBounds(200, 100, 400, 40);
         contentPane.add(withdrawalAmountField);
 
-        withdrawButton = createStyledButton("Withdraw");
-        withdrawButton.setBounds(300, 150, 200, 40);
+        withdrawButton = new JButton("Withdraw");
+        withdrawButton.setFont(customFont);
+        withdrawButton.setBackground(new Color(255, 140, 0));
+        withdrawButton.setForeground(Color.WHITE);
+        withdrawButton.setBounds(250, 175, 150, 40); // Adjusted position for Withdraw button
         withdrawButton.addActionListener(this);
         contentPane.add(withdrawButton);
 
-        int centerX = (800 - 3 * 60) / 2;
-        int centerY = 250;
+        backspaceButton = new JButton("←");
+        backspaceButton.setFont(customFont);
+        backspaceButton.setBackground(primaryColor);
+        backspaceButton.setForeground(Color.WHITE);
+        backspaceButton.setBounds(650, 100, 60, 40);
+        backspaceButton.addActionListener(this);
+        contentPane.add(backspaceButton);
+
+        depositButton = new JButton("Deposit");
+        depositButton.setFont(customFont);
+        depositButton.setBackground(new Color(0, 255, 0));
+        depositButton.setForeground(Color.WHITE);
+        depositButton.setBounds(450, 175, 150, 40); // Adjusted position for Deposit button
+        depositButton.addActionListener(this);
+        contentPane.add(depositButton);
+
 
         digitButtons = new JButton[10];
         for (int i = 0; i < 10; i++) {
-            digitButtons[i] = createStyledButton(String.valueOf(i));
+            digitButtons[i] = new JButton(String.valueOf(i));
+            digitButtons[i].setFont(customFont);
+            digitButtons[i].setBackground(primaryColor);
+            digitButtons[i].setForeground(Color.WHITE);
             digitButtons[i].setBounds(centerX + 60 * (i % 3), centerY + 60 * (i / 3), 60, 60);
             digitButtons[i].addActionListener(this);
             contentPane.add(digitButtons[i]);
         }
     }
 
-    private JButton createStyledButton(String text) {
-        JButton button = new JButton(text);
-        button.setFont(customFont);
-        button.setBackground(primaryColor);
-        button.setForeground(Color.WHITE);
-        return button;
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        String databaseUrl = "jdbc:mysql://localhost:3306/atm%20user"; // Replace space with "%20" in the database name
-        String dbUsername = "root"; // Replace with your database username
-        String dbPassword = ""; // Replace with your database password
+        String databaseUrl = "jdbc:mysql://localhost:3306/atm%20user";
+        String dbUsername = "root";
+        String dbPassword = "";
 
         if (e.getSource() == backButton) {
             LoginPage loginPage = new LoginPage();
             loginPage.setVisible(true);
             dispose();
-        } else if (e.getSource() == withdrawButton) {
+        }else if (e.getSource() == backspaceButton) {
+            if (inputBuffer.length() > 0) {
+                inputBuffer = inputBuffer.substring(0, inputBuffer.length() - 1);
+                withdrawalAmountField.setText(inputBuffer);
+            }
+        }
+        else if (e.getSource() == withdrawButton) {
             try {
                 int withdrawalAmount = Integer.parseInt(inputBuffer);
                 if (withdrawalAmount > balance) {
                     JOptionPane.showMessageDialog(this, "Error...Insufficient funds");
-                   System.exit(0);
+                    System.exit(0);
                 }
-                // Update the user's balance in the database
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 Connection con = DriverManager.getConnection(databaseUrl, dbUsername, dbPassword);
 
@@ -114,26 +141,48 @@ public class Landing2 extends JFrame implements ActionListener {
                 if (rowsUpdated > 0) {
                     balance -= withdrawalAmount;
                     JOptionPane.showMessageDialog(this, "Rs."+withdrawalAmount+" successfully withdrawn . Updated Balance is : " + (balance));
-                    // Update the balance locally
                 } else {
                     JOptionPane.showMessageDialog(this, "Withdrawal failed. Please try again.");
                 }
 
                 con.close();
-            } catch (NumberFormatException ex) {
-                JOptionPane.showMessageDialog(this, "Invalid withdrawal amount. Please enter a valid number.");
-            } catch (Exception ex) {
+            } catch (NumberFormatException | SQLException | ClassNotFoundException ex) {
                 ex.printStackTrace();
                 JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             }
-            // Clear the input buffer
             inputBuffer = "";
             withdrawalAmountField.setText(inputBuffer);
-        } else if (e.getSource()==balanceButton) {
-            Balance bal = new Balance(username,password,balance);
+        } else if (e.getSource() == depositButton) {
+            try {
+                int depositAmount = Integer.parseInt(inputBuffer);
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection con = DriverManager.getConnection(databaseUrl, dbUsername, dbPassword);
+
+                String updateQuery = "UPDATE user SET balance = balance + ? WHERE username = ?";
+                PreparedStatement preparedStatement = con.prepareStatement(updateQuery);
+                preparedStatement.setInt(1, depositAmount);
+                preparedStatement.setString(2, username);
+
+                int rowsUpdated = preparedStatement.executeUpdate();
+
+                if (rowsUpdated > 0) {
+                    balance += depositAmount;
+                    JOptionPane.showMessageDialog(this, "Rs."+depositAmount+" successfully deposited. Updated Balance is: " + (balance));
+                } else {
+                    JOptionPane.showMessageDialog(this, "Deposit failed. Please try again.");
+                }
+
+                con.close();
+            } catch (NumberFormatException | SQLException | ClassNotFoundException ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            inputBuffer = "";
+            withdrawalAmountField.setText(inputBuffer);
+        } else if (e.getSource() == balanceButton) {
+            Balance bal = new Balance(username, password, balance);
             bal.setVisible(true);
-        }
-        else {
+        } else {
             JButton digitButton = (JButton) e.getSource();
             String digit = digitButton.getText();
             inputBuffer += digit;
